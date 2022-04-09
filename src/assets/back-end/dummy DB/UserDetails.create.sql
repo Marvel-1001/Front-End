30caf97d20262921434c4745a197ba482722f043
Qronus <56175454+Qronus@users.noreply.github.com>
2022-04-09 14:52:18 +0200

    added back-end

---
+++ src/assets/back-end/dummy DB/UserDetails.create.sql
@@ -0,0 +1,56 @@
+create table UserDetails (
+	user_id VARCHAR(50),
+	username VARCHAR(50),
+	email VARCHAR(50),
+	profileid VARCHAR(50)
+);
+insert into UserDetails (user_id, username, email, profileid) values ('3C4PDCBB2FT441868', 'tmcphelimy0', 'emathys0@xinhuanet.com', 'https://robohash.org/accusantiumquiavoluptates.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUSF78K49N144475', 'wbrettel1', 'lroffe1@ow.ly', 'https://robohash.org/ullamblanditiisarchitecto.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JHMZE2H51AS046462', 'swilse2', 'ejean2@yahoo.co.jp', 'https://robohash.org/saepeveritatisoccaecati.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WA1WGAFP3DA987610', 'owayt3', 'ocampos3@simplemachines.org', 'https://robohash.org/faceretemporibussimilique.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('2CTALBEW6A6117213', 'cchafer4', 'dbendig4@rambler.ru', 'https://robohash.org/omnisrepellatet.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1C4RDHDG7CC879811', 'lbarensky5', 'bbalasini5@google.fr', 'https://robohash.org/autetconsequatur.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUDL64F85N603697', 'sconvery6', 'qjarville6@flickr.com', 'https://robohash.org/quiaetquis.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('5LMJJ2H54BE568621', 'spinnigar7', 'mvlasyuk7@discuz.net', 'https://robohash.org/distinctioautemquod.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUKD98PX6A729397', 'tsedgwick8', 'cmcgeneay8@sciencedirect.com', 'https://robohash.org/utreprehenderitomnis.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G6KF54982U410380', 'bjerger9', 'bgiacomoni9@ezinearticles.com', 'https://robohash.org/quiadoloremqueculpa.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUSF78E16A434038', 'acracknella', 'nrootea@tamu.edu', 'https://robohash.org/istetemporetenetur.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G6DP577370250553', 'mmacbarronb', 'cplitzb@sohu.com', 'https://robohash.org/nonaporro.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WVWAN7AN1EE278540', 'rdarrellc', 'slipsonc@disqus.com', 'https://robohash.org/voluptatesaepeexplicabo.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G6DP57V480730682', 'lpattinsond', 'rsealeyd@unesco.org', 'https://robohash.org/enimrerumipsam.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1FMEU8FE6AU134056', 'wtaffeee', 'tpargente@ning.com', 'https://robohash.org/atqueullamquaerat.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1FTEW1CF9FF864252', 'rhexamf', 'ddommettf@ucoz.com', 'https://robohash.org/eiusplaceatvoluptatum.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('TRUWT28N431096426', 'eotheng', 'mhazellg@go.com', 'https://robohash.org/maioresvelcupiditate.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JA32X2HU3EU728188', 'nemlochh', 'wedwickeh@vkontakte.ru', 'https://robohash.org/quasetimpedit.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JM1CR2W30A0896835', 'hpresseri', 'fkettridgei@cdbaby.com', 'https://robohash.org/oditsolutaconsectetur.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUNF98P37A228151', 'gfarlambj', 'rbaldonij@alexa.com', 'https://robohash.org/nequenihilexpedita.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G6DS8E30D0400498', 'dbigbyk', 'mcousinsk@marriott.com', 'https://robohash.org/itaquecorruptidelectus.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAULD64B13N620540', 'aflorisl', 'chughlandl@printfriendly.com', 'https://robohash.org/voluptatemexcepturinecessitatibus.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WA1DKAFP8CA402413', 'jedgarsm', 'twoolfordem@godaddy.com', 'https://robohash.org/suntomnissaepe.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1D4PT6GXXAW003534', 'gdewittn', 'rtrebyn@ed.gov', 'https://robohash.org/sequifacereillo.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WBA3R5C51EF444819', 'pthreadgouldo', 'lalbiono@purevolume.com', 'https://robohash.org/sedistereprehenderit.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1C3CCBBBXEN693740', 'aemsonp', 'jritsonp@comsenz.com', 'https://robohash.org/quibusdamconsequaturet.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('4T1BF1FK2EU800641', 'kpicardq', 'dstirtonq@salon.com', 'https://robohash.org/autidet.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WA1DGBFE0CD373283', 'rhuckerber', 'jtinmanr@microsoft.com', 'https://robohash.org/architectoeosperferendis.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1FAHP3EN5BW805227', 'lleefes', 'sclearys@aol.com', 'https://robohash.org/dolornequequis.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JTHFE2C23A2252222', 'tdelytht', 'tgonthiert@mtv.com', 'https://robohash.org/animiexet.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('3VWML7AJ9CM538252', 'arostronu', 'efawsonu@quantcast.com', 'https://robohash.org/liberovoluptatesquas.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G4CU541X14570093', 'aconeronv', 'bpaquetv@soup.io', 'https://robohash.org/eoslaudantiumrerum.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WBAVC73537A654750', 'awillimentw', 'fdallaghanw@imdb.com', 'https://robohash.org/teneturnisivoluptatem.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JN1BY1AP7FM131725', 'gmoutrayreadx', 'ebazelyx@1und1.de', 'https://robohash.org/sitoccaecatiquasi.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('3VW4A7AT5DM229449', 'rjosovichy', 'psnuggsy@ft.com', 'https://robohash.org/adsitin.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WA1DKBFP6CA708547', 'kmatejovskyz', 'mcluderayz@webeden.co.uk', 'https://robohash.org/quiaplaceatconsectetur.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WBAFR9C5XDD541788', 'cmartins10', 'acollingwood10@chron.com', 'https://robohash.org/vitaequiculpa.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('3D73Y3CLXBG268880', 'vdevennie11', 'lbelson11@nifty.com', 'https://robohash.org/nequelaboriosamerror.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1N6AD0CW6EN348367', 'bgadesby12', 'fvamplers12@guardian.co.uk', 'https://robohash.org/utitaquelaudantium.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUEG74FX6N124732', 'dcranton13', 'kbus13@seesaa.net', 'https://robohash.org/excepturirepellendusofficia.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JTDZN3EU7FJ471619', 'rsondon14', 'gfeely14@virginia.edu', 'https://robohash.org/etquonihil.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WAUAF78E55A124131', 'gsaipy15', 'krobilart15@dailymotion.com', 'https://robohash.org/enimetet.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('WBA8Z5C50FG897198', 'dlonnon16', 'kharp16@last.fm', 'https://robohash.org/iureconsequunturnihil.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('5XYKT3A16DG251548', 'dkendle17', 'qchampagne17@reuters.com', 'https://robohash.org/nequedoloresquae.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('1G4HE5EM5AU761013', 'cvanhaeften18', 'edyter18@instagram.com', 'https://robohash.org/esselaboriosamnecessitatibus.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JH4KB16618C930261', 'aburren19', 'pbruggeman19@goodreads.com', 'https://robohash.org/etrecusandaesint.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JTDBT4K35A1957668', 'hticksall1a', 'scollacombe1a@theatlantic.com', 'https://robohash.org/perspiciatiscumaliquid.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JH4CU2F65EC151851', 'rshurmore1b', 'cwaterson1b@bandcamp.com', 'https://robohash.org/voluptatesestcommodi.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('JTEBU5JR0F5912285', 'cemer1c', 'ftailby1c@ow.ly', 'https://robohash.org/blanditiisofficiisrepellat.png?size=50x50&set=set1');
+insert into UserDetails (user_id, username, email, profileid) values ('3C4PDCBB8CT938516', 'nbeamson1d', 'pdegliantoni1d@aboutads.info', 'https://robohash.org/cupiditateveniamut.png?size=50x50&set=set1');
